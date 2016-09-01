7709128bb849a3d0f721e1cfd1a227ae383ba7fd
Guidouil <guidouil@gmail.com>
2016-09-01 11:07:57 +0200

    Search furniturefilter as checkboxes

--- client/templates/search/search_script.js
+++ client/templates/search/search_script.js
@@ -14,6 +14,36 @@ Template.search.helpers({
   now () {
     return moment(new Date()).format('YYYY-MM-DD');
   },
+  wantedFurnitures () {
+    let wantedFurnitures = Session.get('wantedFurnitures');
+    if (wantedFurnitures && wantedFurnitures.length > 0) {
+      let wantedFurnituresIcons = [];
+      _.each( wantedFurnitures, function(furniture) {
+        switch (furniture) {
+        case 'printer':
+          wantedFurnituresIcons.push('<i class="print icon"></i>');
+          break;
+        case 'whiteboard':
+          wantedFurnituresIcons.push('<i class="sticky note outline icon"></i>');
+          break;
+        case 'multiphone':
+          wantedFurnituresIcons.push('<i class="call icon"></i>');
+          break;
+        case 'plugs':
+          wantedFurnituresIcons.push('<i class="plug icon"></i>');
+          break;
+        case 'beamer':
+          wantedFurnituresIcons.push('<i class="record icon"></i>');
+          break;
+        case 'coffee':
+          wantedFurnituresIcons.push('<i class="coffee icon"></i>');
+          break;
+        }
+      });
+      return wantedFurnituresIcons;
+    }
+    return false;
+  },
 });
 
 Template.search.events({
@@ -26,14 +56,19 @@ Template.search.events({
         console.error(error);
       }
       if (result) {
-        // if ($('#furnituresFilterInput').val() != '') {
-        //   let wantedFurnitures = $('#furnituresFilterInput').val().split(',');
-        //   _.each( wantedFurnitures, function(wantedFurniture) {
-        //     result = _.filter(result, function (office) {
-        //       return office.furnitures[wantedFurniture] === true;
-        //     });
-        //   });
-        // }
+        let wantedFurnitures = [];
+        $('.furniture').each(function(index, el) {
+          if (el.checked === true) {
+            wantedFurnitures.push(el.id);
+          }
+        });
+        if (wantedFurnitures.length > 0) {
+          _.each( wantedFurnitures, function(wantedFurniture) {
+            result = _.filter(result, function (office) {
+              return office.furnitures[wantedFurniture] === true;
+            });
+          });
+        }
         Session.set('offices', result);
       }
     });
@@ -51,6 +86,7 @@ Template.search.events({
         wantedFurnitures.push(el.id);
       }
     });
+    Session.set('wantedFurnitures', wantedFurnitures);
     if (wantedFurnitures.length > 0) {
       let offices = Session.get('offices');
       _.each( wantedFurnitures, function(wantedFurniture) {
@@ -66,7 +102,8 @@ Template.search.events({
   'change #dateFilter' () {
     $('.searchOfficesButton').click();
   },
-  'click .furnituresLabel' () {
+  'click .furnituresLabel, click .furnituresFilterSelected' () {
     $('.furnituresFilter').toggle();
+    $('.furnituresFilterSelected').toggle();
   },
 });
