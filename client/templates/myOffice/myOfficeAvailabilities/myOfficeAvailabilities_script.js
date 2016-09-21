0d32398f1498c649dfbf42a21abb0d4f292231d6
Guillaume Darbonne <guidouil@gmail.com>
2016-09-21 13:11:46 +0200

    Add availabilities refactoring

--- client/templates/myOffice/myOfficeAvailabilities/myOfficeAvailabilities_script.js
+++ client/templates/myOffice/myOfficeAvailabilities/myOfficeAvailabilities_script.js
@@ -9,39 +9,54 @@ Template.myOfficeAvailabilities.onCreated(function () {
 
 Template.myOfficeAvailabilities.onRendered(function () {
   let template = this;
-  $('.checkbox').checkbox();
-  let office = Offices.findOne({_id: Router.current().params.officeId});
-  if (office) {
-    let closedDays = [6, 7];
-    if (office.availabilities && office.availabilities.length > 0) {
-      _.each(office.availabilities, function (availability) {
-        closedDays.push(availability.date);
-      });
+  $('.ui.checkbox').checkbox();
+  $('.ui.dropdown').dropdown();
+  Tracker.autorun(function () {
+    let office = Offices.findOne({_id: Router.current().params.officeId});
+    if (office) {
+      let closedDays = [6, 7];
+      if (office.availabilities && office.availabilities.length > 0) {
+        _.each(office.availabilities, function (availability) {
+          closedDays.push(availability.date);
+        });
+      }
+      let reservations = Reservations.find({officeId: Router.current().params.officeId}).fetch();
+      if (reservations && reservations.length > 0) {
+        _.each(reservations, function (reservation) {
+          closedDays.push(reservation.day);
+        });
+      }
+      if (closedDays && closedDays.length > 0) {
+        closedDays.sort(function(a, b){return a-b});
+        let selectedDate = new Date();
+        _.each(closedDays, function (closedDay) {
+          if (moment(closedDay).isSame(selectedDate, 'day')) {
+            selectedDate = moment(selectedDate).add(1, 'd').toDate();
+            if (moment(selectedDate).day() === 6) { // jump the weekend
+              selectedDate = moment(selectedDate).add(2, 'd').toDate();
+            }
+          }
+        });
+        template.closedDays.set(closedDays);
+        $('#startAt').pickadate({
+          firstDay: 1,
+          min: new Date(Session.get('searchedDate')),
+          formatSubmit: 'yyyy-mm-dd',
+          format: 'dd/mm/yyyy',
+          disable: closedDays,
+          select: selectedDate,
+        });
+        $('#endAt').pickadate({
+          firstDay: 1,
+          min: new Date(Session.get('searchedDate')),
+          formatSubmit: 'yyyy-mm-dd',
+          format: 'dd/mm/yyyy',
+          disable: closedDays,
+          select: selectedDate,
+        });
+      }
     }
-    let reservations = Reservations.find({officeId: Router.current().params.officeId}).fetch();
-    if (reservations && reservations.length > 0) {
-      _.each(reservations, function (reservation) {
-        closedDays.push(reservation.day);
-      });
-    }
-    if (closedDays) {
-      template.closedDays.set(closedDays);
-      $('#startAt').pickadate({
-        firstDay: 1,
-        min: new Date(Session.get('searchedDate')),
-        formatSubmit: 'yyyy-mm-dd',
-        format: 'dd/mm/yyyy',
-        disable: closedDays,
-      });
-      $('#endAt').pickadate({
-        firstDay: 1,
-        min: new Date(Session.get('searchedDate')),
-        formatSubmit: 'yyyy-mm-dd',
-        format: 'dd/mm/yyyy',
-        disable: closedDays,
-      });
-    }
-  }
+  });
 });
 
 Template.myOfficeAvailabilities.helpers({
@@ -51,11 +66,42 @@ Template.myOfficeAvailabilities.helpers({
   selectedDate () {
     if (Session.get('searchedDate')) {
       return moment(Session.get('searchedDate')).format('YYYY-MM-DD');
+    } else {
+      let closedDays = Template.instance().closedDays.get();
+      if (closedDays && closedDays.length > 0) {
+        closedDays.sort(function(a, b){return a-b});
+        let selectedDate = new Date();
+        _.each(closedDays, function (closedDay) {
+          if (moment(closedDay).isSame(selectedDate, 'day')) {
+            selectedDate = moment(selectedDate).add(1, 'd').toDate();
+            if (moment(selectedDate).day() === 6) { // jump the weekend
+              selectedDate = moment(selectedDate).add(2, 'd').toDate();
+            }
+          }
+        });
+        return moment(selectedDate).format('YYYY-MM-DD');
+      }
     }
-    return false;
   },
   todayPlaceholder() {
-    return moment(Session.get('searchedDate')).format('DD/MM/YYYY');
+    if (Session.get('searchedDate')) {
+      return moment(Session.get('searchedDate')).format('DD/MM/YYYY');
+    } else {
+      let closedDays = Template.instance().closedDays.get();
+      if (closedDays && closedDays.length > 0) {
+        closedDays.sort(function(a, b){return a-b});
+        let selectedDate = new Date();
+        _.each(closedDays, function (closedDay) {
+          if (moment(closedDay).isSame(selectedDate, 'day')) {
+            selectedDate = moment(selectedDate).add(1, 'd').toDate();
+            if (moment(selectedDate).day() === 6) { // jump the weekend
+              selectedDate = moment(selectedDate).add(2, 'd').toDate();
+            }
+          }
+        });
+        return moment(selectedDate).format('DD/MM/YYYY');
+      }
+    }
   },
   sortDate (availabilities) {
     check(availabilities, Array);
@@ -74,20 +120,23 @@ Template.myOfficeAvailabilities.events({
   'click .addAvailability' (evt, tmpl) {
     $('.field').removeClass('error');
     $('.fields').removeClass('error');
+    $('.ui.form').removeClass('error');
     let startAt = $('[name="startAt_submit"]').val();
     let endAt = $('[name="endAt_submit"]').val();
     if (!startAt) {
-      $('#startAt').parent('.field').addClass('error');
+      $('.startAt').addClass('error');
+      $('.ui.form').addClass('error');
       return false;
     }
     if (!endAt) {
-      $('#endAt').parent('.field').addClass('error');
+      $('.endAt').addClass('error');
+      $('.ui.form').addClass('error');
       return false;
     }
     let duration = 'day';
     $('.duration').each(function(index, el) {
-      if (el.checked === true) {
-        duration = el.value;
+      if ($(el).hasClass('selected')) {
+        duration = el.dataset.value;
       }
     });
     let startTime = '8:00';
@@ -159,6 +208,9 @@ Template.myOfficeAvailabilities.events({
     }, 250);
   },
   'change #startAt' () {
+    $('.field').removeClass('error');
+    $('.fields').removeClass('error');
+    $('.ui.form').removeClass('error');
     let startAtDate = moment($('[name="startAt_submit"]').val()).toDate();
     let endAtPicker = $('#endAt').pickadate('picker');
     endAtPicker.set({
